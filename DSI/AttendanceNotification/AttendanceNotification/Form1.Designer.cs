1cae9b2f95481f650eea373eef4c9b718d7e8750
Jittakorn Srisin <60041997+jinktarot@users.noreply.github.com>
2022-02-01 14:55:24 +0700

    Add files via upload

---
+++ DSI/AttendanceNotification/AttendanceNotification/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace AttendanceNotification
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelAttendance = new System.Windows.Forms.Label();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelAttendance
+            // 
+            this.labelAttendance.AutoSize = true;
+            this.labelAttendance.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelAttendance.Location = new System.Drawing.Point(258, 115);
+            this.labelAttendance.Name = "labelAttendance";
+            this.labelAttendance.Size = new System.Drawing.Size(263, 32);
+            this.labelAttendance.TabIndex = 0;
+            this.labelAttendance.Text = "LINE Attendance Notify";
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonClose.Location = new System.Drawing.Point(335, 192);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(101, 60);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1800000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Auto Close After Complete Attendace";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.labelAttendance);
+            this.Name = "Form1";
+            this.Text = "LINE Attendance Notify";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label labelAttendance;
+        private Button buttonClose;
+        private System.Windows.Forms.Timer timer1;
+        private Label label1;
+    }
+}
---
+++ SystemD&D/AttendanceNotification/AttendanceNotification/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System.Configuration;
+using System.Net;
+using System.Text;
+
+namespace AttendanceNotification
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void lineNotify(string msg)
+        {
+            string token = ConfigurationManager.AppSettings.Get("Token");
+            try
+            {
+                //Initial LINE API
+                var request = (HttpWebRequest)WebRequest.Create("https://notify-api.line.me/api/notify");
+                var postData = string.Format("message={0}", msg);
+                var data = Encoding.UTF8.GetBytes(postData);
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = data.Length;
+                request.Headers.Add("Authorization", "Bearer " + token);
+                using (var stream = request.GetRequestStream()) stream.Write(data, 0, data.Length);
+                var response = (HttpWebResponse)request.GetResponse();
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                //
+            }
+            catch (Exception e)
+            {
+                //Create Log File
+                string LogFilePath = ConfigurationManager.AppSettings.Get("Logfile");
+                using (FileStream FileLog = new FileStream(LogFilePath, FileMode.Append, FileAccess.Write))
+                using (StreamWriter WriteFile = new StreamWriter(FileLog))
+                {
+                    WriteFile.WriteLine(DateTime.Now);
+                    WriteFile.WriteLine(e.Message);
+                    WriteFile.WriteLine("{0} Exception caught.", e);
+                    WriteFile.WriteLine("-----------------------------------------------------------------");
+                }
+            }
+        }
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                //Download File from Google Sheet
+                string Url = "https://docs.google.com/spreadsheets/d/1ERuiSwPufeUyQ4OTpyX-JrQiODwtPVFhAEt5BZgtN24/export?format=csv&gid=1296848798";
+                System.Net.WebClient GoogleSheet = new System.Net.WebClient();
+                byte[] buffer = GoogleSheet.DownloadData(Url);
+                string Path = ConfigurationManager.AppSettings.Get("ReadFilePath");
+                Stream streamFile = new FileStream(Path, FileMode.Create);
+                BinaryWriter writeFile = new BinaryWriter(streamFile);
+                writeFile.Write(buffer);
+                streamFile.Close();
+                string[] GetData = System.IO.File.ReadAllLines(ConfigurationManager.AppSettings.Get("ReadFilePath"));
+                List<string> listofCheckID = System.IO.File.ReadAllLines(ConfigurationManager.AppSettings.Get("ReadFileID")).ToList();
+                //
+                // Max People of System D&D
+                int MaxSystemDD = 32; //DSI=18 / D&D=14
+                int CountSystemDD = 0;
+                var listofID = new List<string>();
+                var listofgetcheckID = new List<string>();
+                // Get date for month format 1-9
+                string DateNowM = DateTime.Now.ToString("d/M/yyyy");
+                // Get date for month format 10-12
+                string DateNowMM = DateTime.Now.ToString("dd/MM/yyyy");
+                foreach (string EachlinesData in GetData)
+                {
+                    string[] GetSplitData = EachlinesData.Split(',');
+                    string GetDate = GetSplitData[2];
+                    if (GetDate == DateNowM || GetDate == DateNowMM)
+                    {
+                        if (EachlinesData.Contains("D&D") || EachlinesData.Contains("DSI") && EachlinesData.Contains(GetDate))
+                        {
+                            string[] CheckDupicateID = EachlinesData.Split(',');
+                            // Add data to list
+                            listofID.Add(CheckDupicateID[3]);
+                        }
+                    }
+                }
+                // Check Duplicate ID
+                if (listofID.Count != listofID.Distinct().Count())
+                {
+                    CountSystemDD = listofID.Distinct().Count();
+                    int ResultCountDD = MaxSystemDD - CountSystemDD;
+                    if (listofID.Distinct().Count() == MaxSystemDD)
+                    {
+                        lineNotify("ประจำวันที่: " + DateNowMM + "\n" + "ทำแบบสำรวจครบแล้ว");
+                        Application.Exit();
+                    }
+                    else if (ResultCountDD < MaxSystemDD)
+                    {
+                        var IDCheckList = listofCheckID.Where(CheckID1 => listofID.All(CheckID2 => CheckID1 != CheckID2));
+                        foreach (string LoopGetID in IDCheckList)
+                        {
+                            listofgetcheckID.Add(LoopGetID);
+                        }
+                        lineNotify("ประจำวันที่: " + DateNowMM + "\n" + "ยังไม่ได้ทำแบบสำรวจอีก: " + listofgetcheckID.Count() + " ท่าน" + "\n" + "พนักงานหมายเลข: " + "\n" +
+                            (String.Join(", ", listofgetcheckID)) + "\n" + "กรุณาทำแบบสำรวจที่: https://forms.gle/wyH5mvAJRnbLA3x47");
+                    }
+                }
+                else
+                {
+                    int ResultCountDD = MaxSystemDD - listofID.Count;
+                    if (listofID.Count == MaxSystemDD)
+                    {
+                        lineNotify("ประจำวันที่: " + DateNowMM + "\n" + "ทำแบบสำรวจครบแล้ว");
+                        Application.Exit();
+                    }
+                    else if (ResultCountDD < MaxSystemDD)
+                    {
+                        var IDCheckList = listofCheckID.Where(CheckID1 => listofID.All(CheckID2 => CheckID1 != CheckID2));
+                        foreach (string LoopGetID in IDCheckList)
+                        {
+                            listofgetcheckID.Add(LoopGetID);
+                        }
+                        lineNotify("ประจำวันที่: " + DateNowMM + "\n" + "ยังไม่ได้ทำแบบสำรวจอีก: " + listofgetcheckID.Count() + " ท่าน" + "\n" + "พนักงานหมายเลข: " + "\n" +
+                            (String.Join(", ", listofgetcheckID)) + "\n" + "กรุณาทำแบบสำรวจที่: https://forms.gle/wyH5mvAJRnbLA3x47");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //Create Log File
+                string LogFilePath = ConfigurationManager.AppSettings.Get("Logfile");
+                using (FileStream FileLog = new FileStream(LogFilePath, FileMode.Append, FileAccess.Write))
+                using (StreamWriter WriteFile = new StreamWriter(FileLog))
+                {
+                    WriteFile.WriteLine(DateTime.Now);
+                    WriteFile.WriteLine(ex.Message);
+                    WriteFile.WriteLine("{0} Exception caught.", ex);
+                    WriteFile.WriteLine("-----------------------------------------------------------------");
+                }
+            }
+        }
+    }
+}
